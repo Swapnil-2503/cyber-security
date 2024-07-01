704f51766ba963ee527051583ca7caea6e1ab51a
techanshuman <46741192+techanshuman@users.noreply.github.com>
2024-07-01 16:19:49 +0530

    updated code for linux

--- Rpi-Server.py
+++ Rpi-Server.py
@@ -1,55 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
-import socket
-import threading
 import subprocess
-
-def get_local_ip_address():
-    try:
-        hostname = socket.gethostname()
-        ip_address = socket.gethostbyname(hostname)
-        return ip_address
-    except socket.gaierror:
-        return None
-
-def start_server():
-    global server_socket
-    # Create a socket object
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        server_socket.bind((get_local_ip_address(), 9999))
-    except Exception as e:
-        update_status(f"Failed to bind socket: {e}")
-        return
-
-    server_socket.listen(5)
-    update_status("Server listening on port 9999")
-
-    try:
-        while True:
-            # Accept a connection
-            client_socket, addr = server_socket.accept()
-            update_status(f"Got a connection from {addr}")
-
-            # Receive data from the client
-            data = client_socket.recv(1024).decode()
-            if not data:
-                break
-            update_status(f"Received '{data}' from the client")
-
-            # Send a response back to the client
-            response = "Echo: " + data
-            client_socket.sendall(response.encode())
-            update_status(f"Sent '{response}' to the client")
-
-            # Close the client socket
-            client_socket.close()
-
-    except Exception as e:
-        update_status(f"Server error: {e}")
-    finally:
-        # Close the server socket
-        server_socket.close()
+import threading
 
 def update_status(message):
     status_text.config(state=tk.NORMAL)
@@ -88,7 +40,6 @@ root.resizable(False, True)
 main_frame = ttk.Frame(root, padding=10)
 main_frame.pack(expand=True, fill=tk.BOTH)
 
-
 command_prompt_label = ttk.Label(main_frame, text="Type commands:")
 command_prompt_label.pack()
 
@@ -99,8 +50,5 @@ command_entry.bind('<Return>', on_command_enter)
 
 status_text = tk.Text(main_frame, wrap=tk.WORD, state=tk.DISABLED, height=20)
 status_text.pack(fill=tk.BOTH)
-
-# start_button = ttk.Button(main_frame, text="Start Server", command=on_start_button)
-# start_button.pack()
 
 root.mainloop()
